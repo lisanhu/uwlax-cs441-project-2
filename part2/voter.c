034b552a824abb6cfa8133a10577cfdb5fb1e2ae
Sanhu Li <li.sanh@uwlax.edu>
2014-10-20 13:38:50 -0500

    Finish coding part2

--- part2/voter.c
+++ part2/voter.c
@@ -1,12 +1,258 @@
 /*
  *
- *
+ * Author: Sanhu Li
  * CS 441/541: Finicky Voter (Assignment 2 Part 2)
- *
+ * Oct. 20, 2014
  */
 #include "voter.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
+
+    int nums[4] = {10, 5, 5, 5};
+    int i;
+
+    if (argc <= 5) {
+        for (i = 1; i < argc; ++i) {
+            nums[i - 1] = strtol(argv[i], NULL, 10);
+        }
+
+        num_booth = nums[0];
+        num_repub = nums[1];
+        num_democ = nums[2];
+        num_indep = nums[3];
+        start_booth();
+    } else {
+        usage();
+    }
 
     return 0;
+}
+
+void usage() {
+    printf("Usage:\n");
+    printf("./start_booth [booth num] [repub num] [democ num] [indep num]\n");
+    fflush(stdout);
+}
+
+
+void start_booth() {
+    int before_open = 2;
+    long total = num_repub + num_democ + num_indep;
+    int i;
+
+    which = 'I';
+    // voters in sequence: rep, dem, ind
+    pthread_t *voters = (pthread_t *) malloc(sizeof(pthread_t) * total);
+    voter_t *confs = (voter_t *) malloc(sizeof(voter_t) * total);
+
+    booth = (char *) malloc(sizeof(char) * num_booth);
+    for (i = 0; i < num_booth; ++i) {
+        booth[i] = '.';
+    }
+
+    semaphore_create(&sem_booth, num_booth);
+    semaphore_create(&station_lock, 1);
+    semaphore_create(&mutex, 1);
+    semaphore_create(&lock, 1);
+    srand(time(NULL));
+
+    semaphore_wait(&station_lock);
+    print_configure(num_booth, num_repub, num_democ, num_indep);
+
+    /** Create voters **/
+    for (i = 0; i < num_repub; ++i) {
+        confs[i].id = i;
+        confs[i].type = 'R';
+        pthread_create(&voters[i], NULL, voter, (void *) &(confs[i]));
+    }
+
+    for (i = 0; i < num_democ; ++i) {
+        int idx = i + num_repub;
+        confs[idx].id = i;
+        confs[idx].type = 'D';
+        pthread_create(&voters[idx], NULL, voter, (void *) &(confs[idx]));
+    }
+
+    for (i = 0; i < num_indep; ++i) {
+        int idx = i + num_repub + num_democ;
+        confs[idx].id = i;
+        confs[idx].type = 'I';
+        pthread_create(&voters[idx], NULL, voter, (void *) &(confs[idx]));
+    }
+    /** Sleep a while **/
+    sleep(before_open);
+    print_slashes();
+    semaphore_post(&station_lock);
+
+    /** Wait until everyone finishes voting **/
+    for (i = 0; i < total; ++i) {
+        pthread_join(voters[i], NULL);
+    }
+    print_slashes();
+    semaphore_destroy(&station_lock);
+    semaphore_destroy(&sem_booth);
+    semaphore_destroy(&lock);
+    semaphore_destroy(&mutex);
+}
+
+void *voter(void *arg) {
+    voter_t v = *(voter_t *) arg;
+    int regi_time = rand() % 50000, vote_time = rand() % 100000;
+    char thread_name[25], thread_with_booth[25];
+    char *message, *buf_str;
+    int i;
+
+    if ('I' == v.type) {
+        sprintf(thread_name, "Independent %4d       ", v.id);
+    } else if ('R' == v.type) {
+        sprintf(thread_name, "Republican  %4d       ", v.id);
+    } else if ('D' == v.type) {
+        sprintf(thread_name, "Democrat    %4d       ", v.id);
+    }
+
+    /** Waiting in front of polling station **/
+    message = "Waiting for polling station to open...";
+    buf_str = buffer2str(booth, num_booth);
+    printf("%s %s %s\n", thread_name, buf_str, message);
+    free(buf_str);
+    fflush(stdout);
+
+    semaphore_wait(&station_lock);
+    semaphore_post(&station_lock);
+    /** The station opened **/
+    message = "Entering the polling station";
+    buf_str = buffer2str(booth, num_booth);
+    printf("%s %s %s\n", thread_name, buf_str, message);
+    free(buf_str);
+    fflush(stdout);
+
+    /** Registration **/
+    usleep(regi_time);
+
+    /** Try entering a booth **/
+    semaphore_wait(&sem_booth);
+    if ('I' != v.type) {
+
+        if ('R' == v.type) {
+            while (true) {
+                semaphore_wait(&lock);
+                if ('D' != which || findNo('D')) {
+                    which = 'R';
+                    semaphore_post(&lock);
+                    break;
+                } else {
+                    semaphore_post(&lock);
+                }
+            }
+        }
+
+        if ('D' == v.type) {
+            while (true) {
+                semaphore_wait(&lock);
+                if ('R' != which || findNo('R')) {
+                    which = 'D';
+                    semaphore_post(&lock);
+                    break;
+                } else {
+                    semaphore_post(&lock);
+                }
+            }
+        }
+    }
+
+    semaphore_wait(&mutex);
+    i = findBooth();
+    if (-1 != i) {
+        booth[i] = v.type;
+    } else {
+        fprintf(stderr, "Error!!! Cannot find a booth!!!\n");
+        exit(-1);
+    }
+
+    if ('I' == v.type) {
+        sprintf(thread_with_booth, "Independent %4d in %2d ", v.id, i);
+    } else if ('R' == v.type) {
+        sprintf(thread_with_booth, "Republican  %4d in %2d ", v.id, i);
+    } else if ('D' == v.type) {
+        sprintf(thread_with_booth, "Democrat    %4d in %2d ", v.id, i);
+    }
+
+    message = "Voting!";
+    buf_str = buffer2str(booth, num_booth);
+    printf("%s %s %s\n", thread_with_booth, buf_str, message);
+    free(buf_str);
+    fflush(stdout);
+    semaphore_post(&mutex);
+
+    /** Voting process **/
+    usleep(vote_time);
+
+    /** Leaving the polling station **/
+
+    semaphore_wait(&mutex);
+    booth[i] = '.';
+
+    message = "Leaving the polling station";
+    buf_str = buffer2str(booth, num_booth);
+    printf("%s %s %s\n", thread_name, buf_str, message);
+    free(buf_str);
+    fflush(stdout);
+    semaphore_post(&mutex);
+    semaphore_post(&sem_booth);
+
+    return NULL;
+}
+
+void print_configure(const int booth, const int repub, const int democ, const int indep) {
+    printf("Number of Voting Booths:%4d\n", booth);
+    printf("Number of Republican   :%4d\n", repub);
+    printf("Number of Democrat     :%4d\n", democ);
+    printf("Number of Independent  :%4d\n", indep);
+    print_slashes();
+    fflush(stdout);
+}
+
+void print_slashes() {
+    int len = num_booth * 3 + 9;
+    int i;
+    char slashes[len];
+
+    for (i = 0; i < len; ++i) {
+        slashes[i] = '-';
+    }
+    slashes[len - 1] = '\0';
+
+    printf("------------------------+%s+--------------------------------\n", slashes);
+}
+
+char *buffer2str(char *buf, int num) {
+    int len = num * 3 + 11;
+    char str[len];
+    int i;
+    sprintf(str, "|->  ");
+    for (i = 0; i < num; ++i) {
+        sprintf(str, "%s[%c]", str, buf[i]);
+    }
+    sprintf(str, "%s  <-|", str);
+    return strdup(str);
+}
+
+int findBooth() {
+    int i;
+    for (i = 0; i < num_booth; ++i) {
+        if ('.' == booth[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool findNo(char type) {
+    int i;
+    for (i = 0; i < num_booth; ++i) {
+        if (type == booth[i]) {
+            return false;
+        }
+    }
+    return true;
 }
--- part2/voter.h
+++ part2/voter.h
@@ -1,8 +1,8 @@
 /*
  *
- *
+ * Author: Sanhu Li
  * CS 441/541: Finicky Voter (Assignment 2 Part 2)
- *
+ * Oct. 20, 2014
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +11,8 @@
 #include <errno.h>
 #include <pthread.h>
 #include "semaphore_support.h"
+#include "stdbool.h"
+#include "string.h"
 
 /*****************************
  * Defines
@@ -20,13 +22,40 @@
 /*****************************
  * Structures
  *****************************/
-
+typedef struct {
+    int id;
+    char type;
+} voter_t;
 
 /*****************************
  * Global Variables
  *****************************/
-
+char * booth;
+char which;
+int num_booth;
+int num_repub;
+int num_democ;
+int num_indep;
+semaphore_t sem_booth;
+semaphore_t station_lock;
+semaphore_t mutex;
+semaphore_t lock;
 
 /*****************************
  * Function Declarations
  *****************************/
+void usage();
+
+void start_booth();
+
+void * voter(void *arg);
+
+void print_configure(const int booth, const int repub, const int democ, const int indep);
+
+char * buffer2str(char * buf, int num);
+
+void print_slashes();
+
+int findBooth();
+
+bool findNo(char type);
